d08219df3407629df2bf3d549e40885311f0b3e1
Origami ONOUE <onoueorigami@gmail.com>
2020-02-04 02:15:29 +0900

    fix: more easy to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 
 const useHelloWorld = () => {
-    const [str, setStr] = useState("");
+    const [str, setStr] = useState("work in progress...");
     useEffect(() => {
         setTimeout(() => {
             setStr("hello world!");
-        });
+        }, 4000);
     });
 
     return str;
