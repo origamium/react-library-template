1490a7569b035bd49cc661b8b171c00c96639b76
Origami ONOUE <onoueorigami@gmail.com>
2020-02-04 00:49:00 +0900

    chore: add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 6,
+        project: "./tsconfig.json",
+        sourceType: "module",
+        createDefaultProgram: true
+    },
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
+    ],
+    plugins: ["@typescript-eslint", "react-hooks"],
+    settings: {
+        react: {
+            version: "detect"
+        },
+        node: {
+            tryExtensions: [".ts", ".tsx", ".js", ".jsx", ".json", ".node"]
+        }
+    },
+    overrides: [
+        {
+            files: ["**/*.ts", "**/*.tsx"],
+            rules: {
+                "no-console": ["warn", {allow: ["info", "warn", "error"]}],
+                "no-unused-vars": 0,
+                "@typescript-eslint/no-unused-vars": [2, { args: "none" }],
+                "@typescript-eslint/no-require-imports": 2,
+                "@typescript-eslint/no-var-requires": 2,
+                "@typescript-eslint/consistent-type-assertions": 2,
+                "@typescript-eslint/no-unnecessary-type-assertion": 2,
+                "@typescript-eslint/restrict-plus-operands": 2,
+                '@typescript-eslint/return-await': 'error',
+                "react-hooks/rules-of-hooks": "error",
+                "react-hooks/exhaustive-deps": "warn",
+                "@typescript-eslint/explicit-function-return-type": "off",
+                "react/prop-types": "off"
+            }
+        },
+        {
+            files: ["**/__tests__/**", "**/*.test.tsx", "**/*.test.ts"],
+            env: {
+                jest: true
+            },
+            rules: {
+                "no-invalid-this": 0
+            }
+        }
+    ]
+}
