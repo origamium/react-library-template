4145ac81648a485307fcee612f33beca2d62fc0e
origamium <onoueorigami@gmail.com>
2020-04-04 09:56:21 +0900

    chore: addon-viewportの追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
     stories: ["../src/**/*.stories.tsx"],
-    addons: ["@storybook/addon-actions", "@storybook/addon-links"],
+    addons: ["@storybook/addon-actions", "@storybook/addon-links", "@storybook/addon-viewport"],
     webpackFinal: async config => {
         // do mutation to the config
         config.module.rules.push({
@@ -16,5 +16,5 @@ module.exports = {
         });
         config.resolve.extensions.push(".ts", ".tsx");
         return config;
-    }
+    },
 };
