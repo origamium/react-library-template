55fed20e74b82f7645d490f2f945ce98ae2b9b1e
origamium <onoueorigami@gmail.com>
2020-04-04 09:41:57 +0900

    fix: 通常テストがエラーでコケる不具合の修正

--- jest/test/jest.config.js
+++ jest/test/jest.config.js
@@ -4,11 +4,13 @@ module.exports = {
     transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
+    testEnvironment: "jsdom",
     testRegex: "./*\\.(test|spec)\\.tsx?$",
     moduleNameMapper: {
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/jest/fileMock.js",
         "\\.(css|less)$": "<rootDir>/jest/styleMock.js"
     },
-    moduleFileExtensions: ["js", "ts", "tsx"]
+    setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+    moduleFileExtensions: ["js", "ts", "tsx", "json"]
 };
